a39ab587d5d541ad628623cd0dea55809787d650
PRAVESH PANDEY <58443282+pravesh-pandey@users.noreply.github.com>
2021-05-22 19:09:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 
-var rand1=Math.floor(Math.random()*6)+1;
-var rand2=Math.floor(Math.random()*6)+1;
-var rand_img1="images/dice"+rand1+".png";
-var rand_img2="images/dice"+rand2+".png";
+run_dice();
+document.querySelector("button").addEventListener("click",free_run);
+function free_run()
+{
+  run_dice();
+}
+function run_dice() {
+  var rand1 = Math.floor(Math.random() * 6) + 1;
+  var rand2 = Math.floor(Math.random() * 6) + 1;
+  var rand_img1 = "images/dice" + rand1 + ".png";
+  var rand_img2 = "images/dice" + rand2 + ".png";
 
-var change1=document.querySelectorAll("img")[0];
-var change2=document.querySelectorAll("img")[1];
+  var change1 = document.querySelectorAll("img")[0];
+  var change2 = document.querySelectorAll("img")[1];
 
-change1.setAttribute("src",rand_img1);
-change2.setAttribute("src",rand_img2);
+  change1.setAttribute("src", rand_img1);
+  change2.setAttribute("src", rand_img2);
 
-if(rand1>rand2)
-document.querySelector( "h1").innerHTML="Player 1 Wins✨";
-else if(rand2>rand1)
-document.querySelector( "h1").innerHTML="Player 2 Wins✨";
-else
-document.querySelector( "h1").innerHTML="Its a Draw ";
+  if (rand1 > rand2)
+    document.querySelector("h1").innerHTML = "Player 1 Wins✨";
+  else if (rand2 > rand1)
+    document.querySelector("h1").innerHTML = "Player 2 Wins✨";
+  else
+    document.querySelector("h1").innerHTML = "Its a Draw ";
+}
